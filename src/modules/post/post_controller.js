6437f5c62d7b4d47ca4e0bcdb652ce5b637442b3
Shaaban <shabanhamdy94@gmail.com>
2024-08-28 19:45:31 +0300

    first commit

--- src/modules/post/post_controller.js
+++ src/modules/post/post_controller.js
@@ -6,41 +6,22 @@ import postModel from "../../../db/models/post_modal.js"
 
 
 export const createPost = async(req,res,next)=>{
-    
-    
-    
     if (!req.files) {
         next(new Error("please select post picture", { cause: 400 }))
     }
-
     const post = await postModel.create({
-        // postImage: req.files?.postImage.map((e) =>"https://freshcard-ecommerce-node.onrender.com/" + e.path),
         postImage: req.files?.postImage.map((e) =>"https://shaaban-facebook-node.up.railway.app/" + e.path),
         postContent: req.body.postContent,
         likes: req.body.likes,
         unLikes: req.body.unLikes,
         postTime: new Date(),
         userId:req.user.id
-        
-
     })
    if (!post) {
     
      return  next(new Error("fail to create post"))
    }
    res.json({message:"success",post})
-
-}
-
-
-
-// ======================================================
-
-export const deletePost = async (req, res, next) => {
-    const post = await postModel.deleteOne({_id:req.body.post_id})
-    if (post.deletedCount < 1) return next(new Error("there not find andy posts ", 
-        { cause: 409 }))
-    res.status(201).json({ message: "success", deletedCount: post.deletedCount })
 }
 
 
@@ -60,3 +41,11 @@ export const getAllPosts = async (req, res, next) => {
 }
 
 
+// ======================================================
+
+export const deletePost = async (req, res, next) => {
+    const post = await postModel.deleteOne({_id:req.body.post_id})
+    if (post.deletedCount < 1) return next(new Error("there not find andy posts ", 
+        { cause: 409 }))
+    res.status(201).json({ message: "success", deletedCount: post.deletedCount })
+}
--- src/modules/post/post_routes.js
+++ src/modules/post/post_routes.js
@@ -8,14 +8,15 @@ const router = Router()
 
 
 
-// router.post("/createProduct", myMulter()
-//     .fields([{ name: "mainImage", maxCount: 1 }, { name: "subImages", maxCount: 5 }]),
-//     asyncHandling(productConnection.createProduct))
-
-
 router.post("/createPost",auth(),myMulter()
 .fields([{ name: "postImage", maxCount: 1 }]),
 asyncHandling(post_controllers.createPost))
+
+router.get("/getAllPosts",auth(),asyncHandling(post_controllers.getAllPosts))
+
+
+router.delete("/deletePost",auth(),
+asyncHandling(post_controllers.deletePost))
 
 
 
